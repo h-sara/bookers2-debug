671b1d90f5feec84286d8038eb762a5d59fd7114
h-sara <115357536+h-sara@users.noreply.github.com>
2022-11-30 14:40:54 +0000

    [Break] bookers2-debug

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,9 @@ class Book < ApplicationRecord
   has_many :user
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+
+  has_one_attached :image
+  def get_image
+    (image.attached?) ? image : 'no_image.jpg'
+  end
 end

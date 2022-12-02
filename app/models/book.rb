14a71ef6512f34f2cf183083f7990b025885e05d
h-sara <115357536+h-sara@users.noreply.github.com>
2022-12-02 07:33:25 +0000

    [Start] task3

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :user
+  has_many :favorites, dependent: :destroy
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 
@@ -7,4 +8,9 @@ class Book < ApplicationRecord
   def get_image
     (image.attached?) ? image : 'no_image.jpg'
   end
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
 end

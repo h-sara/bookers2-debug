549609183455bb452c57a435ab2b30b7def6278a
h-sara <115357536+h-sara@users.noreply.github.com>
2022-12-03 07:36:36 +0000

    [Break] task4

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # フォローした、されたの関係
+  has_many :relationship_er, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :relationship_ed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 一覧画面で使用する
+  has_many :followings, through: :relationship_er, source: :followed
+  has_many :followers, through: :relationship_ed, source: :follower
 
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
--- db/migrate/20221203053915_create_relationships.rb
+++ db/migrate/20221203053915_create_relationships.rb
@@ -1,7 +1,8 @@
 class CreateRelationships < ActiveRecord::Migration[6.1]
   def change
     create_table :relationships do |t|
-
+      t.integer :follower_id #フォローしたユーザのID
+      t.integer :followed_id #フォローされたユーザのID
       t.timestamps
     end
   end

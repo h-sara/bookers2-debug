5b8d992c7d90ac8712fdae223d9df8c8634e830b
h-sara <115357536+h-sara@users.noreply.github.com>
2022-12-03 08:39:46 +0000

    [Break] task4

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,16 @@ class User < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+  # フォローした時の処理
+  def follow(user_id)
+    relationship_er.create(followed_id: user_id)
+  end
+  # フォローを外すときの処理
+  def unfollow(user_id)
+    relationship_er.find_by(followed_id: user_id).destroy
+  end
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+  end
 end

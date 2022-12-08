8d47081c98e94834ca9de2267c892fc0d1be5e47
h-sara <115357536+h-sara@users.noreply.github.com>
2022-12-08 11:55:59 +0000

    [Start] task6

---
+++ app/javascript/script.js
@@ -0,0 +1,9 @@
+/*global $*/
+// $(document).redy(function() {
+//   $('.favorite').on('click', function() {
+//     $(this).addClass('text-primary');
+//   });
+//   $('.favorite').on('click', function() {
+//     $(this).addClass('text-danger');
+//   });
+// });
